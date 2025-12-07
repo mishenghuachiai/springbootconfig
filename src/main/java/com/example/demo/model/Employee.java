3f65023a6e9747d84653b57fb60e47a95c251722
mishenghuachiai <stonegkk@gmail.com>
2025-12-07 22:19:10 +0800

    add:增加自动产生代码实体类

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -4,8 +4,6 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
-import java.lang.reflect.Method;
-
 
 @Data
 public class Employee {
---
+++ src/main/java/com/example/demo/utils/EntityGenerator.java
@@ -0,0 +1,38 @@
+package com.example.demo.utils;
+
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
+
+public class EntityGenerator {
+
+    public static void main(String[] args) {
+        // 1. 数据库连接信息（改成你的）
+        String url = "jdbc:mysql://localhost:3306/mp?useSSL=true&useUnicode=true&characterEncoding=utf8&serverTimezone=Asia/Shanghai";
+        String username = "root";
+        String password = "123456";
+
+        FastAutoGenerator.create(url, username, password)
+                // 全局配置
+                .globalConfig(builder -> {
+                    builder.author("Morris") // 作者
+                            .dateType(DateType.TIME_PACK)
+                            .outputDir("E://code"); // 输出目录（你可以改）
+                })
+                // 包配置
+                .packageConfig(builder -> {
+                    builder.parent("com.example")    // 父包
+                            .entity("domain.entity"); // 生成实体类的包路径
+                })
+
+                // 策略配置：只生成实体类
+                .strategyConfig(builder -> {
+                    builder.addInclude("employee") // 你要生成的表列表（多个表可写多个）
+                            .entityBuilder()
+                            .enableLombok()        // 开启 Lombok
+                            .enableChainModel()    // 链式模型（可选）
+                            .enableTableFieldAnnotation(); // 字段加注解 @TableField
+                })
+                .execute(); // 执行
+    }
+}
+
